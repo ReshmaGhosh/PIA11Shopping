990e01f240ece97fcf916379554c74e54c394808
ReshmaGhosh <reshmaghosh114@gmail.com>
2022-10-24 09:08:44 +0200

    Initial Commit

---
+++ PIA11Shopping/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PIA11Shopping
+//
+//  Created by Reshma Ghosh on 2022-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
